b97d9a8197a139e0420f28c71d2cd29ffadeb22a
lesleyc@bu.edu <lesleyc@bu.edu>
2020-11-08 13:05:19 +0800

    finished with about page

---
+++ model/content/philosopher.js
@@ -0,0 +1,74 @@
+let philosopher = [
+    {
+        name: "Ibn Sina",
+        date: "1990-01-01",
+        printedDate: "980 - 1037",
+        image: "https://www.dropbox.com/s/v0z49npumxafid4/ibn-sina.jpg?raw=1",
+        description:
+            "Ibn Sina was a successful doctor in Persia (modern day Iran) that believed that everyone had two parts to themselves: a part that everyone could see and another part - which he called the soul - often times, we only see a little bit of someone - we may know someone from their behaviors, but we don't really get a full picture of who they really are.",
+    },
+    {
+        name: "Ludwig Wittgenstein",
+        date: "2019-01-01",
+        printedDate: "1889-1950",
+        image:
+            "https://www.dropbox.com/s/efeb31gyyyeskiw/d1006b228277269187b9b04b338d6881.jpg?raw=1",
+        description:
+            "Ludwig Wittgenstein was a German philosopher who was born in 1889. He was very wealthy but also enjoyed doing many different jobs: he was a gardener for a while, he cleaned hospitals, he taught at a school and even designed a house for his sister. He really enjoyed spending time by himself, and was very interested in how words can explain ourselves better. When we describe and explain something carefully, we may have a better chance of getting other people to understand what we really mean. When someone does not understand us, instead of getting frustrated, we could try to explain and describe more slowly and carefully and see if that helps.",
+    },
+    {
+        name: "Albert Camus",
+        date: "2020-01-01",
+        printedDate: "1913-1960",
+        image:
+            "https://www.dropbox.com/s/nwyq2gh79yk1gu7/Albert_Camus%252C_gagnant_de_prix_Nobel%252C_portrait_en_buste%252C_pos%25C3%25A9_au_bureau%252C_faisant_face_%25C3%25A0_gauche%252C_cigarette_de_tabagisme.jpg?raw=1",
+        description:
+            "Albert Camus was a French philosopher that was born in 1913; he wrote many books and even played on the Algerian football team. He not only won the Nobel prize in 1957, but was also extremely good-looking and had many girlfriends.\nWe may sometimes insult someone by saying that they are not “normal” - but Camus believed that it is normal to be quite weird - everyone is much more strange and interesting than they initially seem - everyone has weird thoughts and secrets that they don’t tell anyone else about, too. People also act differently, too, when they are around different people - that’s because the person that they are when they are with you is only a small part of who they are. Camus believed that we should care a little less about what other people thought, and that would make us less worried, less lonely, and more confident in ourselves.",
+    },
+    {
+        name: "Rene Descartes",
+        date: "2017-01-01",
+        printedDate: "1596-1650",
+        image:
+            "https://www.dropbox.com/s/tc6o67qzhl4c9kr/Top-questions-answers-Rene-Descartes.jpg?raw=1",
+        description:
+            "Rene Descartes was a French philosopher who was born in 1596 and really enjoyed staying in bed during the mornings to think. His friends thought he was lazy, though he wasn’t, really - taking time to think deeply about how to solve problems can be hard work. He believed that it was difficult for anyone to really know anything for sure - people frequently act as if they know all kinds of things when they don’t, really. People seem to know what a good job is, how things should work, how you should act and what you should do with your free time. But people don’t really know the best ideas to these problems. They aren’t stupid - it’s just that these questions are so hard that we may sometimes have difficulty finding the right answer for us.",
+    },
+    {
+        name: "Jean Paul Satre",
+        date: "2018-01-01",
+        printedDate: "1905-1980",
+        image:
+            "https://www.dropbox.com/s/wf8midiagtq4t77/ap_437220028307-e1526067604790.jpg?raw=1",
+        description:
+            "Jean Paul Satre was a philosopher who was born in 1905. He frequently got bullied in school and after university, became a school teacher. He really enjoyed going to cafes and eating cakes and pastries in his free time. Satre was fascinated with the weird things about life - one of these strange things was that we can experience our own lives very strongly - we have our own ideas, anxieties and thoughts in our own head, but we may simply be the girl who lives down the street to someone else. Satre was worried that we may begin to lose the strange and weird thoughts we have in our heads when we care too much about how other people think of us. We don’t do this on purpose, but we can sometimes also forget how interesting and fascinating other people can be - you can’t really know someone by the way they look - you really have to get to know them first.",
+    },
+    {
+        name: "Michel De Montaigne",
+        date: "2016-01-01",
+        printedDate: "1533-1592",
+        image:
+            "https://www.dropbox.com/s/14d3odkv0x82voo/550px-Michel_de_Montaigne_1.jpg?raw=1",
+        description:
+            "Michel De Montaigne was a French philosopher who was born in 1533. He came from a rich family and really enjoyed reading the various books he had in his tower. He thought you could learn a lot from doing simple things in life, such as growing vegetables or cleanin ga house. He got the opportunity to travel, which opened his eyes to how different people can act and behave in various enviornments. We may sometimes feel strange or different from the people around us because we might not be similar to them - thought we mgiht feel like we fit right in elsewhere. Then, he did an extremely interesting thing - he wrote a book about what it was like to be him. He wrote out all the things that he liked and found interesting and to his surprise, a lot of people also liked it. It was really only one out of a hundred that really enjoyed his writing - so it might not be people that lived around him, but he found out something extremely important: there are lots of people out there that can understand you - you might just not know them yet.",
+    },
+    {
+        name: "Mary Wollstonecraft",
+        date: "2018-10-11",
+        printedDate: "1759-1797",
+        image: "https://www.dropbox.com/s/pg34ibzf3xr5usn/p02kcm0t.jpg?raw=1",
+        description:
+            "Mary Wollstonecraft was an Enlgish philosopher who was born in 1759. When she became older, her and her sisters opened a school, which caused quite a scandal, as people only thought that boys could get an education at the time. She was quite brave, too - and was not too worried about what other people thought of her. Mary was fascinated with how people spend money - she wanted people to really think before they spent their money. She felt like a lot of rich people spent money on things that they did not really like. It is not that she thought it was good to be poor - she liked wearing nice clothes and was happy from making money from the books that she wrote - she just wanted people to realize that nice and simple things can be very nice, too.",
+    },
+    {
+        name: "Philosophy",
+        date: "2020-01-01",
+        printedDate: "Today",
+        image:
+            "https://www.dropbox.com/s/286ucdiyv7inzmi/%2522The_School_of_Athens%2522_by_Raffaello_Sanzio_da_Urbino.jpg?raw=1",
+        description:
+            "Philosophy is one of the best ways we can deal with the problems in our lives - one of the main ways that it helps is that it provides us the tools to bravely think about how we are living our lives, and potentially anticipate the things that may go wrong. If you are able to see the problems in your life as various challenges, you can begin to develop skills that help you tackle those obstacles. Philosophy addresses the problems that face in adult life - and tries to make things easier than they are - it’s not frightened about how tough and hard life can sometimes feel - it has already done it a bunch of times before.",
+    },
+];
+
+module.exports = { philosopher };
